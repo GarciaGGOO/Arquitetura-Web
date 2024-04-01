be0428ff9f3ece0939a59e2088f02a0e28dec134
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2024-04-01 19:37:59 -0300

    Add files via upload

---
+++ demo/src/test/java/Exercicio_aula_3/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Exercicio_aula_3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
