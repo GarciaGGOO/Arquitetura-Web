72bbf771fe6936d3e4116139b550d6f7550ca95a
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2024-04-01 20:07:02 -0300

    Add files via upload

--- demo/src/main/java/exercicio_aula_3/demo/controller/AlunoController.java
+++ demo/src/main/java/exercicio_aula_3/demo/controller/AlunoController.java
@@ -1,65 +1,45 @@
 package exercicio_aula_3.demo.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import exercicio_aula_3.demo.modelo.Aluno;
+import exercicio_aula_3.demo.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import exercicio_aula_3.demo.Aluno;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ex3")
+@RequestMapping("/api/alunos")
 public class AlunoController {
 
-    private List<Aluno> alunos = new ArrayList<>();
+    private final AlunoService alunoService;
 
-    public AlunoController() {
-
-        alunos.add(new Aluno("João", "Silva", 20, 'M', "Engenharia"));
-        alunos.add(new Aluno("Mariana", "Santos", 22, 'F', "Ciência da Computação"));
-        alunos.add(new Aluno("Portari", "Souza", 19, 'M', "Medicina"));
-        alunos.add(new Aluno("Matheusa", "Ferreira", 21, 'F', "Administração"));
-        alunos.add(new Aluno("Icaro", "Oliveira", 23, 'M', "Direito"));
+    @Autowired
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
     }
 
     @GetMapping
     public List<Aluno> getAllAlunos() {
-        return alunos;
+        return alunoService.getAllAlunos();
     }
 
-    @GetMapping("/alunos/{index}")
+    @GetMapping("/{index}")
     public Aluno getAlunoByIndex(@PathVariable int index) {
-        if (index >= 0 && index < alunos.size()) {
-            return alunos.get(index);
-        } else {
-            return null;
-        }
+        return alunoService.getAlunoByIndex(index);
     }
 
     @PostMapping("/add")
-    public Aluno createAluno(@RequestBody Aluno aluno) {
-        alunos.add(aluno);
-        System.out.println("Aluno adicionado: " + aluno);
-        return aluno;
+    public void createAluno(@RequestBody Aluno aluno) {
+        alunoService.createAluno(aluno);
     }
 
-    @PutMapping("/alunos/{index}")
-    public Aluno updateAluno(@PathVariable int index, @RequestBody Aluno aluno) {
-        if (index >= 0 && index < alunos.size()) {
-            alunos.set(index, aluno);
-            System.out.println("Atualização: " + aluno);
-            return aluno;
-        } else {
-            System.out.println("Aluno não encontrado");
-            return null;
-        }
+    @PutMapping("/{index}")
+    public void updateAluno(@PathVariable int index, @RequestBody Aluno aluno) {
+        alunoService.updateAluno(index, aluno);
     }
 
-    @DeleteMapping("/alunos/{index}")
+    @DeleteMapping("/{index}")
     public void deleteAluno(@PathVariable int index) {
-        if (index >= 0 && index < alunos.size()) {
-            alunos.remove(index);
-        }
+        alunoService.deleteAluno(index);
     }
 }
